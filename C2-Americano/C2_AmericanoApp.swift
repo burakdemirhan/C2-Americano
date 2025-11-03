9038a6a5ae6acf37b851ad02f88c1c59c1e740c8
Burak Demirhan <burkidad@outlook.com>
2025-11-03 20:36:21 +0100

    Initial SwiftUI project

---
+++ C2-Americano/C2_AmericanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2_AmericanoApp.swift
+//  C2-Americano
+//
+//  Created by Burak Demirhan on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct C2_AmericanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C2-Americano/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C2-Americano
+//
+//  Created by Burak Demirhan on 03/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
